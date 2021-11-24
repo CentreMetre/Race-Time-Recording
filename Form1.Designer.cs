4a560e688d306d9a98bbcd82d2abcbf4d983a4f5
Martin McLaren <61330791+LuckyMiner01@users.noreply.github.com>
2021-11-24 16:57:13 +0000

    Added dynamic text boxes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,6 @@ namespace Winforms_App
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_toggle_dark_mode = new System.Windows.Forms.Button();
             this.radbtn_male = new System.Windows.Forms.RadioButton();
             this.radbtn_female = new System.Windows.Forms.RadioButton();
             this.listbox_racers_amount = new System.Windows.Forms.ListBox();
@@ -59,17 +58,18 @@ namespace Winforms_App
             this.lbl_lane_7 = new System.Windows.Forms.Label();
             this.lbl_lane_8 = new System.Windows.Forms.Label();
             this.lbl_sorted_times = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btn_sort = new System.Windows.Forms.Button();
+            this.lbl_test = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // btn_toggle_dark_mode
-            // 
-            this.btn_toggle_dark_mode.Location = new System.Drawing.Point(9, 266);
-            this.btn_toggle_dark_mode.Name = "btn_toggle_dark_mode";
-            this.btn_toggle_dark_mode.Size = new System.Drawing.Size(75, 34);
-            this.btn_toggle_dark_mode.TabIndex = 0;
-            this.btn_toggle_dark_mode.Text = "Toggle Dark Mode";
-            this.btn_toggle_dark_mode.UseVisualStyleBackColor = true;
-            this.btn_toggle_dark_mode.Click += new System.EventHandler(this.btn_toggle_dark_mode_Click);
             // 
             // radbtn_male
             // 
@@ -81,6 +81,7 @@ namespace Winforms_App
             this.radbtn_male.TabStop = true;
             this.radbtn_male.Text = "Male";
             this.radbtn_male.UseVisualStyleBackColor = true;
+            this.radbtn_male.CheckedChanged += new System.EventHandler(this.radbtn_male_CheckedChanged_1);
             // 
             // radbtn_female
             // 
@@ -92,9 +93,11 @@ namespace Winforms_App
             this.radbtn_female.TabStop = true;
             this.radbtn_female.Text = "Female";
             this.radbtn_female.UseVisualStyleBackColor = true;
+            this.radbtn_female.CheckedChanged += new System.EventHandler(this.radbtn_female_CheckedChanged_1);
             // 
             // listbox_racers_amount
             // 
+            this.listbox_racers_amount.Enabled = false;
             this.listbox_racers_amount.FormattingEnabled = true;
             this.listbox_racers_amount.Items.AddRange(new object[] {
             "4",
@@ -131,56 +134,64 @@ namespace Winforms_App
             // 
             // txtbox_time_7
             // 
-            this.txtbox_time_7.Location = new System.Drawing.Point(313, 205);
+            this.txtbox_time_7.Enabled = false;
+            this.txtbox_time_7.Location = new System.Drawing.Point(274, 205);
             this.txtbox_time_7.Name = "txtbox_time_7";
             this.txtbox_time_7.Size = new System.Drawing.Size(100, 20);
             this.txtbox_time_7.TabIndex = 18;
             // 
             // txtbox_time_8
             // 
-            this.txtbox_time_8.Location = new System.Drawing.Point(313, 231);
+            this.txtbox_time_8.Enabled = false;
+            this.txtbox_time_8.Location = new System.Drawing.Point(274, 231);
             this.txtbox_time_8.Name = "txtbox_time_8";
             this.txtbox_time_8.Size = new System.Drawing.Size(100, 20);
             this.txtbox_time_8.TabIndex = 19;
             // 
             // txtbox_time_6
             // 
-            this.txtbox_time_6.Location = new System.Drawing.Point(313, 179);
+            this.txtbox_time_6.Enabled = false;
+            this.txtbox_time_6.Location = new System.Drawing.Point(274, 179);
             this.txtbox_time_6.Name = "txtbox_time_6";
             this.txtbox_time_6.Size = new System.Drawing.Size(100, 20);
             this.txtbox_time_6.TabIndex = 17;
             // 
             // txtbox_time_5
             // 
-            this.txtbox_time_5.Location = new System.Drawing.Point(313, 153);
+            this.txtbox_time_5.Enabled = false;
+            this.txtbox_time_5.Location = new System.Drawing.Point(274, 153);
             this.txtbox_time_5.Name = "txtbox_time_5";
             this.txtbox_time_5.Size = new System.Drawing.Size(100, 20);
             this.txtbox_time_5.TabIndex = 16;
             // 
             // txtbox_time_3
             // 
-            this.txtbox_time_3.Location = new System.Drawing.Point(313, 101);
+            this.txtbox_time_3.Enabled = false;
+            this.txtbox_time_3.Location = new System.Drawing.Point(274, 101);
             this.txtbox_time_3.Name = "txtbox_time_3";
             this.txtbox_time_3.Size = new System.Drawing.Size(100, 20);
             this.txtbox_time_3.TabIndex = 14;
             // 
             // txtbox_time_4
             // 
-            this.txtbox_time_4.Location = new System.Drawing.Point(313, 127);
+            this.txtbox_time_4.Enabled = false;
+            this.txtbox_time_4.Location = new System.Drawing.Point(274, 127);
             this.txtbox_time_4.Name = "txtbox_time_4";
             this.txtbox_time_4.Size = new System.Drawing.Size(100, 20);
             this.txtbox_time_4.TabIndex = 15;
             // 
             // txtbox_time_2
             // 
-            this.txtbox_time_2.Location = new System.Drawing.Point(313, 75);
+            this.txtbox_time_2.Enabled = false;
+            this.txtbox_time_2.Location = new System.Drawing.Point(274, 75);
             this.txtbox_time_2.Name = "txtbox_time_2";
             this.txtbox_time_2.Size = new System.Drawing.Size(100, 20);
             this.txtbox_time_2.TabIndex = 13;
             // 
             // txtbox_time_1
             // 
-            this.txtbox_time_1.Location = new System.Drawing.Point(313, 49);
+            this.txtbox_time_1.Enabled = false;
+            this.txtbox_time_1.Location = new System.Drawing.Point(274, 49);
             this.txtbox_time_1.Name = "txtbox_time_1";
             this.txtbox_time_1.Size = new System.Drawing.Size(100, 20);
             this.txtbox_time_1.TabIndex = 12;
@@ -188,7 +199,7 @@ namespace Winforms_App
             // lbl_times
             // 
             this.lbl_times.AutoSize = true;
-            this.lbl_times.Location = new System.Drawing.Point(310, 32);
+            this.lbl_times.Location = new System.Drawing.Point(271, 32);
             this.lbl_times.Name = "lbl_times";
             this.lbl_times.Size = new System.Drawing.Size(87, 13);
             this.lbl_times.TabIndex = 21;
@@ -213,7 +224,7 @@ namespace Winforms_App
             // lbl_lanes
             // 
             this.lbl_lanes.AutoSize = true;
-            this.lbl_lanes.Location = new System.Drawing.Point(226, 9);
+            this.lbl_lanes.Location = new System.Drawing.Point(187, 9);
             this.lbl_lanes.Name = "lbl_lanes";
             this.lbl_lanes.Size = new System.Drawing.Size(39, 13);
             this.lbl_lanes.TabIndex = 24;
@@ -222,7 +233,7 @@ namespace Winforms_App
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(310, 9);
+            this.label1.Location = new System.Drawing.Point(271, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(142, 13);
             this.label1.TabIndex = 25;
@@ -230,10 +241,10 @@ namespace Winforms_App
             // 
             // btn_help
             // 
-            this.btn_help.Location = new System.Drawing.Point(90, 277);
+            this.btn_help.Location = new System.Drawing.Point(9, 264);
             this.btn_help.Name = "btn_help";
-            this.btn_help.Size = new System.Drawing.Size(44, 23);
-            this.btn_help.TabIndex = 26;
+            this.btn_help.Size = new System.Drawing.Size(44, 35);
+            this.btn_help.TabIndex = 1;
             this.btn_help.Text = "Help";
             this.btn_help.UseVisualStyleBackColor = true;
             this.btn_help.Click += new System.EventHandler(this.btn_help_Click);
@@ -241,7 +252,7 @@ namespace Winforms_App
             // lbl_lane_1
             // 
             this.lbl_lane_1.AutoSize = true;
-            this.lbl_lane_1.Location = new System.Drawing.Point(226, 52);
+            this.lbl_lane_1.Location = new System.Drawing.Point(187, 52);
             this.lbl_lane_1.Name = "lbl_lane_1";
             this.lbl_lane_1.Size = new System.Drawing.Size(43, 13);
             this.lbl_lane_1.TabIndex = 27;
@@ -250,7 +261,7 @@ namespace Winforms_App
             // lbl_lane_2
             // 
             this.lbl_lane_2.AutoSize = true;
-            this.lbl_lane_2.Location = new System.Drawing.Point(226, 78);
+            this.lbl_lane_2.Location = new System.Drawing.Point(187, 78);
             this.lbl_lane_2.Name = "lbl_lane_2";
             this.lbl_lane_2.Size = new System.Drawing.Size(43, 13);
             this.lbl_lane_2.TabIndex = 28;
@@ -259,7 +270,7 @@ namespace Winforms_App
             // lbl_lane_3
             // 
             this.lbl_lane_3.AutoSize = true;
-            this.lbl_lane_3.Location = new System.Drawing.Point(226, 104);
+            this.lbl_lane_3.Location = new System.Drawing.Point(187, 104);
             this.lbl_lane_3.Name = "lbl_lane_3";
             this.lbl_lane_3.Size = new System.Drawing.Size(43, 13);
             this.lbl_lane_3.TabIndex = 29;
@@ -268,7 +279,7 @@ namespace Winforms_App
             // lbl_lane_4
             // 
             this.lbl_lane_4.AutoSize = true;
-            this.lbl_lane_4.Location = new System.Drawing.Point(226, 130);
+            this.lbl_lane_4.Location = new System.Drawing.Point(187, 130);
             this.lbl_lane_4.Name = "lbl_lane_4";
             this.lbl_lane_4.Size = new System.Drawing.Size(43, 13);
             this.lbl_lane_4.TabIndex = 30;
@@ -277,7 +288,7 @@ namespace Winforms_App
             // lbl_lane_5
             // 
             this.lbl_lane_5.AutoSize = true;
-            this.lbl_lane_5.Location = new System.Drawing.Point(226, 156);
+            this.lbl_lane_5.Location = new System.Drawing.Point(187, 156);
             this.lbl_lane_5.Name = "lbl_lane_5";
             this.lbl_lane_5.Size = new System.Drawing.Size(43, 13);
             this.lbl_lane_5.TabIndex = 31;
@@ -286,7 +297,7 @@ namespace Winforms_App
             // lbl_lane_6
             // 
             this.lbl_lane_6.AutoSize = true;
-            this.lbl_lane_6.Location = new System.Drawing.Point(226, 182);
+            this.lbl_lane_6.Location = new System.Drawing.Point(187, 182);
             this.lbl_lane_6.Name = "lbl_lane_6";
             this.lbl_lane_6.Size = new System.Drawing.Size(43, 13);
             this.lbl_lane_6.TabIndex = 32;
@@ -295,7 +306,7 @@ namespace Winforms_App
             // lbl_lane_7
             // 
             this.lbl_lane_7.AutoSize = true;
-            this.lbl_lane_7.Location = new System.Drawing.Point(226, 208);
+            this.lbl_lane_7.Location = new System.Drawing.Point(187, 208);
             this.lbl_lane_7.Name = "lbl_lane_7";
             this.lbl_lane_7.Size = new System.Drawing.Size(43, 13);
             this.lbl_lane_7.TabIndex = 33;
@@ -304,7 +315,7 @@ namespace Winforms_App
             // lbl_lane_8
             // 
             this.lbl_lane_8.AutoSize = true;
-            this.lbl_lane_8.Location = new System.Drawing.Point(226, 234);
+            this.lbl_lane_8.Location = new System.Drawing.Point(187, 234);
             this.lbl_lane_8.Name = "lbl_lane_8";
             this.lbl_lane_8.Size = new System.Drawing.Size(43, 13);
             this.lbl_lane_8.TabIndex = 34;
@@ -313,18 +324,127 @@ namespace Winforms_App
             // lbl_sorted_times
             // 
             this.lbl_sorted_times.AutoSize = true;
-            this.lbl_sorted_times.Location = new System.Drawing.Point(499, 9);
+            this.lbl_sorted_times.Location = new System.Drawing.Point(460, 9);
             this.lbl_sorted_times.Name = "lbl_sorted_times";
             this.lbl_sorted_times.Size = new System.Drawing.Size(64, 13);
             this.lbl_sorted_times.TabIndex = 35;
             this.lbl_sorted_times.Text = "A sorted list:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(460, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Lane 8:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(460, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 42;
+            this.label3.Text = "Lane 7:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(460, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Lane 6:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(460, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 13);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Lane 5:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(460, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 39;
+            this.label6.Text = "Lane 4:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(460, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 13);
+            this.label7.TabIndex = 38;
+            this.label7.Text = "Lane 3:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(460, 77);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 37;
+            this.label8.Text = "Lane 2:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(460, 51);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 36;
+            this.label9.Text = "Lane 1:";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(59, 264);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(354, 35);
+            this.label10.TabIndex = 44;
+            // 
+            // btn_sort
+            // 
+            this.btn_sort.Location = new System.Drawing.Point(380, 229);
+            this.btn_sort.Name = "btn_sort";
+            this.btn_sort.Size = new System.Drawing.Size(48, 23);
+            this.btn_sort.TabIndex = 45;
+            this.btn_sort.Text = "Sort";
+            this.btn_sort.UseVisualStyleBackColor = true;
+            this.btn_sort.Click += new System.EventHandler(this.btn_sort_Click);
+            // 
+            // lbl_test
+            // 
+            this.lbl_test.AutoSize = true;
+            this.lbl_test.Location = new System.Drawing.Point(597, 81);
+            this.lbl_test.Name = "lbl_test";
+            this.lbl_test.Size = new System.Drawing.Size(41, 13);
+            this.lbl_test.TabIndex = 46;
+            this.lbl_test.Text = "label11";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(741, 311);
+            this.ClientSize = new System.Drawing.Size(741, 305);
+            this.Controls.Add(this.lbl_test);
+            this.Controls.Add(this.btn_sort);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.lbl_sorted_times);
             this.Controls.Add(this.lbl_lane_8);
             this.Controls.Add(this.lbl_lane_7);
@@ -354,7 +474,7 @@ namespace Winforms_App
             this.Controls.Add(this.listbox_racers_amount);
             this.Controls.Add(this.radbtn_female);
             this.Controls.Add(this.radbtn_male);
-            this.Controls.Add(this.btn_toggle_dark_mode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -364,8 +484,6 @@ namespace Winforms_App
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btn_toggle_dark_mode;
         private System.Windows.Forms.RadioButton radbtn_male;
         private System.Windows.Forms.RadioButton radbtn_female;
         private System.Windows.Forms.ListBox listbox_racers_amount;
@@ -395,6 +513,17 @@ namespace Winforms_App
         private System.Windows.Forms.Label lbl_lane_7;
         private System.Windows.Forms.Label lbl_lane_8;
         private System.Windows.Forms.Label lbl_sorted_times;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btn_sort;
+        private System.Windows.Forms.Label lbl_test;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -31,6 +31,14 @@ namespace Winforms_App
         //Minimum racers = 4
         //Maximum racers = 8
 
+        //array for input boxes names
+        String[] input_box_names = { "txtbox_time_1", "txtbox_time_2", "txtbox_time_3", "txtbox_time_4", "txtbox_time_5", "txtbox_time_6", "txtbox_time_7", "txtbox_time_8" };
+        /*for (int i = 0; i <= 7; i++)
+        {
+            int j = i + 1;
+            input_box_names[i] = "txtbox_time_" + j;
+        }*/
+
         public Form1()
         {
             InitializeComponent();
@@ -38,8 +46,9 @@ namespace Winforms_App
 
         public void Form1_Load(object sender, EventArgs e)
         {
+            //string input_box_name_array = "txtbox_time_";
             //bool dark_mode_enabled = false;
-
+            lbl_test.Text = input_box_names[1];
         }
 
         private void lbl_output_Click(object sender, EventArgs e)
@@ -68,19 +77,122 @@ namespace Winforms_App
 
         private void listbox_racers_amount_SelectedIndexChanged(object sender, EventArgs e)
         {
-            /*for (int i = 0; i < )
+            //trying to enbale them using arryas, not worked
+            /*for (int i = 0; i <= Convert.ToInt32(listbox_racers_amount.SelectedItem); i++)
             {
+                int j = i + 1;
+                input_box_names[i] = "txtbox_time_" + j + ".Enabled";
+
+
 
             }*/
+            //lbl_test.Text = listbox_racers_amount.SelectedItem;
+
+            //hard coding it :(
+            int racers_amount = Convert.ToInt32(listbox_racers_amount.SelectedItem);
+            if (racers_amount == 4)
+            {
+                txtbox_time_1.Enabled = true;
+                txtbox_time_2.Enabled = true;
+                txtbox_time_3.Enabled = true;
+                txtbox_time_4.Enabled = true;
+
+                //disabling
+                txtbox_time_5.Enabled = false;
+                txtbox_time_6.Enabled = false;
+                txtbox_time_7.Enabled = false;
+                txtbox_time_8.Enabled = false;
+            }
+            else if (racers_amount == 5)
+            {
+                txtbox_time_1.Enabled = true;
+                txtbox_time_2.Enabled = true;
+                txtbox_time_3.Enabled = true;
+                txtbox_time_4.Enabled = true;
+                txtbox_time_5.Enabled = true;
+
+                //disabling
+                txtbox_time_6.Enabled = false;
+                txtbox_time_7.Enabled = false;
+                txtbox_time_8.Enabled = false;
+            }
+            else if (racers_amount == 6)
+            {
+                txtbox_time_1.Enabled = true;
+                txtbox_time_2.Enabled = true;
+                txtbox_time_3.Enabled = true;
+                txtbox_time_4.Enabled = true;
+                txtbox_time_5.Enabled = true;
+                txtbox_time_6.Enabled = true;
+
+                //disabling
+                txtbox_time_7.Enabled = false;
+                txtbox_time_8.Enabled = false;
+            }
+            else if (racers_amount == 7)
+            {
+                txtbox_time_1.Enabled = true;
+                txtbox_time_2.Enabled = true;
+                txtbox_time_3.Enabled = true;
+                txtbox_time_4.Enabled = true;
+                txtbox_time_5.Enabled = true;
+                txtbox_time_6.Enabled = true;
+                txtbox_time_7.Enabled = true;
+
+                //disabling
+                txtbox_time_8.Enabled = false;
+            }
+            else if (racers_amount == 8)
+            {
+                txtbox_time_1.Enabled = true;
+                txtbox_time_2.Enabled = true;
+                txtbox_time_3.Enabled = true;
+                txtbox_time_4.Enabled = true;
+                txtbox_time_5.Enabled = true;
+                txtbox_time_6.Enabled = true;
+                txtbox_time_7.Enabled = true;
+                txtbox_time_8.Enabled = true;
+            }
         }
 
         private void btn_help_Click(object sender, EventArgs e)
         {
+            //Chose the sex of the racers and how many racers there are. Then input their time in the corrosponing lane input box. Then press the 'sort' button.
+
             if (help_enabled == false)
             {
 
             }
 
+        }
+
+        private void btn_sort_Click(object sender, EventArgs e)
+        {
+            if (radbtn_male.Checked == true)
+            {
+                bool racer_is_male = true;
+            }
+            else
+            {
+                bool racer_is_male = false;
+            }
+            /*
+            for (int i = 0; i < listbox_racers_amount.SelectedIndex; i++)
+            {
+                txtbox_time_i = txtbox_time_1 + i;
+            }*/ // was for not allowing inputs on boxes th user didnt want, not sure how to do it 
+
+            Array[] input_box_names = new Array[8];
+        }
+
+        private void radbtn_female_CheckedChanged_1(object sender, EventArgs e)
+        {
+            listbox_racers_amount.Enabled = true;
+        }
+
+        private void radbtn_male_CheckedChanged_1(object sender, EventArgs e)
+        {
+            listbox_racers_amount.Enabled = true;
         }
     }
 }
